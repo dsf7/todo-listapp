4b24876b7831ab24787d4d0d349198187bd64325
Gabriel Rizzo <daudtrizzo@gmail.com>
2022-04-15 16:53:47 -0300

    #

--- scripts/services/logout.js
+++ scripts/services/logout.js
@@ -1,6 +1,7 @@
 const signOut = document.getElementById("closeApp");
 const logout = (e) => {
   // dps de fazer o logout, deve-se apagar o token do localStorage
+  console.log("Logout");
   window.localStorage.removeItem("jwt");
   window.location.href = "index.html";
 };
